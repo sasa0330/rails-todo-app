417f522e806708757611f9f3c1a6a87dd6f7b5e3
sasa0330 <sasa0330y@gmail.com>
2024-12-20 10:48:38 +0900

    消す

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -21,17 +21,7 @@ class TodosController < ApplicationController
 
   # POST /todos or /todos.json
   def create
-    @todo = Todo.new(todo_params)
 
-    respond_to do |format|
-      if @todo.save
-        format.html { redirect_to @todo, notice: "Todo was successfully created." }
-        format.json { render :show, status: :created, location: @todo }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /todos/1 or /todos/1.json
